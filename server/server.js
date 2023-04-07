997f39a84f7ea44142c2194f2af1e8880de2d8f8
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-07 17:35:17 -0500

    Built calculator function in server.js

--- server/server.js
+++ server/server.js
@@ -34,3 +34,25 @@ app.post('/thing', (req, res) => {
 app.listen(5000, function () {
    console.log(`You started the server! It is running on port 5000.`);
 })
+
+
+function calculator(num1, num2, operator) {
+   let answer;
+
+   if (operator === 'add') {
+      answer = (num1 + num2)
+   }// End add
+   else if (operator === 'subtract') {
+      answer = (num1 - num2)
+   }// End subtract
+   else if (operator === 'multiply') {
+      answer = (num1 * num2)
+   }// End multiply
+   else if (operator === 'divide') {
+      answer = (num1 / num2)
+   }// End divide
+   else {
+      answer = `Bad info received. num1--> ${num1}, num2--> ${num2}, operator --> '${operator}'`;
+   }// End undefined operator
+   return answer;
+}// End calculator
